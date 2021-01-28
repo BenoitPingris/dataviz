ee4a01991de0593c2633cfd5246d37c6dc0e66ce
Benoit Pingris <benoit.pingris@pm.me>
2021-01-28 15:44:31 +0100

    update get date

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ filename = 'data.csv'
 
 def read_file():
     with open(filename, 'r') as fd:
-        reader = csv.reader(fd, delimiter=';')
+        reader = csv.reader(fd)
         next(reader)
         d = []
         # Car;MPG;Cylinders;Displacement;Horsepower;Weight;Acceleration;Model;Origin
         for row in reader:
             d.append({
-                'car': row[0],
-                'mpg': row[1],
-                'cylinders': row[2],
-                'displacement': row[3],
-                'hp': row[4],
-                'weight': row[5],
-                'acc': row[6],
-                'model': row[7],
-                'origin': row[8],
+                'id': row[0],
+                'car': row[1],
+                'mpg': row[2],
+                'cylinders': row[3],
+                'displacement': row[4],
+                'hp': row[5],
+                'weight': row[6],
+                'acc': row[7],
+                'model': row[8],
+                'origin': row[9],
                 })
         return d
 
